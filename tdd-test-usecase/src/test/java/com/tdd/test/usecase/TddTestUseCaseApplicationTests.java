a6d6af75bbe8f306be668b0c0dccda20d8e63f7e
paulosneto <paulo_sneto@yahoo.com>
2024-11-26 11:08:04 -0300

    feat: created standard test class

---
+++ tdd-test-usecase/src/test/java/com/tdd/test/usecase/TddTestUseCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.test.usecase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddTestUseCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
