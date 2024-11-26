1abf343a3bb2a4dcdc83a5b28860c1af55fed7b9
paulosneto <paulo_sneto@yahoo.com>
2024-11-26 11:07:51 -0300

    feat: created standard class

---
+++ tdd-test-usecase/src/main/java/com/tdd/test/usecase/TddTestUseCaseApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.test.usecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddTestUseCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddTestUseCaseApplication.class, args);
+	}
+
+}
